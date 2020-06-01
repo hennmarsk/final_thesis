955110b197866e7f0b2e4480c5c45a953cab518f
tung <tungdarkknight@gmail.com>
2020-06-01 11:32:44 +0700

    update model final layer's dimensions

--- nn/my_model.py
+++ nn/my_model.py
@@ -3,5 +3,5 @@ import tensorflow.keras.applications as applications
 
 def create_model(input_shape):
     model = applications.MobileNetV2(
-        weights=None, input_shape=input_shape, classes=512)
+        weights=None, input_shape=input_shape, classes=128)
     return model
